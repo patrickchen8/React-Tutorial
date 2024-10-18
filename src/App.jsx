4ed8a3138f10aa5ada7d0c214893759945021b83
patrickchen8 <patrick.chen0459@gmail.com>
2024-10-18 16:32:42 -0500

    Added Firebase Database

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,24 @@
 import Banner from './components/Banner.jsx'
 import MainPage from './pages/MainPage.jsx'
 import FormPage from './pages/FormPage.jsx'
-import {useJsonQuery} from './utilities/fetch.js'
+import { useDbData } from './utilities/firebase.js'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 
 
 const App = () => {
-  const url = 'https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php';
-  const [schedule, isLoading, error] = useJsonQuery(url);
+  const [data, error] = useDbData('/');
 
-
-  if(error) {
-    return (<h1 className="text-2xl font-bold text-red-600 text-center"> AN ERROR OCCURED!</h1>);
-  }
-  if(isLoading) {
-    return (<></>);
-  }
-  if(!schedule) {
-    return (<h1 className="text-2xl font-bold text-red-600 text-center"> NO DATA FOUND!</h1>);
-  }
+  if (error) return <h1 className="text-red-500 text-2xl text-center">Error loading data: {error.toString()}</h1>;
+  if (data === undefined) return <h1 className="text-purple-400 text-2xl text-center">Loading data...</h1>;
+  if (!data) return <h1 className="text-red-500 text-2xl text-center">No data found</h1>;
 
 
   return (<>
-    <Banner title={schedule.title}/>
+    <Banner title={data.title}/>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainPage courses={schedule.courses}/>} />
+        <Route path="/" element={<MainPage courses={data.courses}/>} />
         <Route path="/edit/:courseId" element={<FormPage />} />
       </Routes>
     </BrowserRouter>
